2bb18dfba593063fe6143ffcca328cb62750767f
DarKRs <centralstreet_24@mail.ru>
2023-04-28 21:01:12 +0600

    Ходы коня

--- MoveGenerators/KnightMoveGenerator.cs
+++ MoveGenerators/KnightMoveGenerator.cs
@@ -11,6 +11,40 @@ namespace ChessRebirth.MoveGenerators
 {
     internal class KnightMoveGenerator : IPieceMoveGenerator
     {
+        public override List<Move> GetValidMoves(Piece knight, bool checkForCheck, int? toX = null, int? toY = null)
+        {
+            List<Move> validMoves = new List<Move>();
 
+            int[] offsetsX = { 2, 1, -1, -2, -2, -1, 1, 2 };
+            int[] offsetsY = { 1, 2, 2, 1, -1, -2, -2, -1 };
+
+            for (int i = 0; i < offsetsX.Length; i++)
+            {
+                int newX = knight.PositionX + offsetsX[i];
+                int newY = knight.PositionY + offsetsY[i];
+
+                if (!IsInBounds(newX, newY))
+                {
+                    continue;
+                }
+
+                Move move = new Move { FromX = knight.PositionX, FromY = knight.PositionY, ToX = newX, ToY = newY, TargetPiece = knight.Board.GetPiece(newX, newY) };
+
+                if (move.TargetPiece == null || move.TargetPiece.Color != knight.Color)
+                {
+                    if (!checkForCheck || !IsKingInCheckAfterMove(knight, newX, newY))
+                    {
+                        validMoves.Add(move);
+                    }
+                }
+
+                if (toX.HasValue && toY.HasValue && newX == toX.Value && newY == toY.Value)
+                {
+                    break;
+                }
+            }
+
+            return validMoves;
+        }
     }
 }
